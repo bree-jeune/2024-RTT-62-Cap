a868acbb064f2531a11227384790ba07cf8e15a1
bree-jeune <bree.j.32@gmail.com>
2024-07-22 07:57:42 -0700

    added Spring Boot files

---
+++ Capstone/src/main/java/com/ParamedicRepository/Capstone/ParamedicScenarioRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.ParamedicRepository.Capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParamedicScenarioRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParamedicScenarioRepositoryApplication.class, args);
+	}
+
+}
---
+++ Capstone/src/test/java/com/ParamedicRepository/Capstone/ParamedicScenarioRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ParamedicRepository.Capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParamedicScenarioRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
